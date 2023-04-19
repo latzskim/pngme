1932e0743c20ce47b9754ffa2e16dba2d1b024be
latzskim <karolmiksztal@gmail.com>
2023-04-19 20:29:02 +0200

    feat: add clap and play with base functionalities of this library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-mod chunk_type;
+use clap::{ Command, Arg};
+
 mod chunk;
+mod chunk_type;
 mod png;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("pngme")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("encode")
+                .about("encodes data in the chunk")
+                .arg(Arg::new("path").required(true).help("path to png file"))
+                .arg(Arg::new("type").required(true).help("valid chunk type e.g ruSt"))
+                .arg(Arg::new("data").required(false).help("data to be encoded"))
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("encode", encode_matches)) => {
+            let path = encode_matches
+                .get_one::<String>("path")
+                .map(|s| s.as_str())
+                .expect("path is required");
+
+            let chunk_type = encode_matches
+                .get_one::<String>("type")
+                .map(|s| s.as_str())
+                .expect("type is required");
+
+
+            let chunk_data = encode_matches
+                .get_one::<String>("data")
+                .map(|s| s.as_str())
+                .unwrap_or("");
+
+            println!("command: {} {} {}", path, chunk_type, chunk_data);
+        }
+        _ => panic!("oh shieet"),
+    }
 }
